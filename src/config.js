2ba725e280c95443cc9bce4c7c3bf77445f36da8
liuwk <1834183435@qq.com>
2019-09-20 17:07:53 +0800

    dd

--- src/config.js
+++ src/config.js
@@ -4,4 +4,6 @@
 export const client_id = 'test';
 export const client_secret = "test";
 // export const  BASE_API = 'https://rapapi.renqilai.com/app/mock/55';
-export const  BASE_API = 'http://192.168.0.222:8088';
+// export const  BASE_API = 'http://192.168.0.222:8088';
+
+export const BASE_API = "https://quxiaapi.renqilai.com"
